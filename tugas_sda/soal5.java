a9a1e7094ed4b57e04de30ebb01b6e400fbda2e3
anggarmadi <89975341+anggarmadi@users.noreply.github.com>
2022-02-27 21:45:22 +0700

    sesuaiken dengan soal dan hapus bagian bawah

--- tugas_sda/soal5.java
+++ tugas_sda/soal5.java
@@ -10,29 +10,15 @@ public class soal5 {
         nama.add("d");
         nama.add("i");
 
-        nama.remove(3);
+        nama.remove(0);
         System.out.println(nama);
 
         nama.remove(2);
         System.out.println(nama);
 
-        nama.remove(0);
+        nama.remove(3);
         System.out.println(nama);
 
-        nama.add(0, "e");
-        System.out.println(nama);
-        nama.add(2, "f");
-        System.out.println(nama);
-        nama.add(3, "g");
-        System.out.println(nama);
-        nama.add(4, "h");
-        System.out.println(nama);
-        nama.add(6, "h");
-        System.out.println(nama);
-        nama.add(-3, "j");
-        System.out.println(nama);
-
-
-        
+           
     }
 }
